e598e83f126a3a000388f35f23db60761f00d79c
Yuri Schimke <yuri@schimke.ee>
2018-12-09 21:23:32 +0000

    Basic Access Logs

---
+++ src/main/kotlin/com/baulsupp/cooee/ktor/AccessLogs.kt
@@ -0,0 +1,51 @@
+package com.baulsupp.cooee.ktor
+
+import com.baulsupp.cooee.honeyClient
+import io.ktor.application.Application
+import io.ktor.application.ApplicationCall
+import io.ktor.application.ApplicationCallPipeline
+import io.ktor.application.ApplicationFeature
+import io.ktor.application.call
+import io.ktor.request.httpMethod
+import io.ktor.request.httpVersion
+import io.ktor.request.path
+import io.ktor.request.userAgent
+import io.ktor.util.AttributeKey
+import io.ktor.util.pipeline.PipelinePhase
+
+class AccessLogs {
+  class Configuration {
+  }
+
+  companion object Feature : ApplicationFeature<Application, AccessLogs.Configuration, AccessLogs> {
+    override val key: AttributeKey<AccessLogs> = AttributeKey("Access Logs")
+
+    override fun install(pipeline: Application, configure: AccessLogs.Configuration.() -> Unit): AccessLogs {
+      val loggingPhase = PipelinePhase("Logging")
+      val feature = AccessLogs()
+
+      pipeline.insertPhaseBefore(ApplicationCallPipeline.Monitoring, loggingPhase)
+
+      pipeline.intercept(loggingPhase) {
+        proceed()
+        feature.logSuccess(call)
+      }
+
+      return feature
+    }
+  }
+
+  private fun logSuccess(call: ApplicationCall) {
+    val dataMap = mapOf(
+      "path" to call.request.path(),
+      "method" to call.request.httpMethod.value,
+      "result" to call.response.status()?.value,
+      "useragent" to call.request.userAgent(),
+      "httpversion" to call.request.httpVersion
+    )
+
+    if (honeyClient != null) {
+      honeyClient!!.createEvent().addFields(dataMap).setDataset("accesslogs").send()
+    }
+  }
+}
