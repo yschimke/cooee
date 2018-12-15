50e1efe74c2f76b3071c435fee75bb17547e336a
Yuri Schimke <yuri@schimke.ee>
2018-12-15 11:11:57 +0000

    Remove dead code

--- src/main/kotlin/com/baulsupp/cooee/providers/Provider.kt
+++ src/main/kotlin/com/baulsupp/cooee/providers/Provider.kt
@@ -8,8 +8,6 @@ import com.baulsupp.cooee.completion.SimpleArgumentCompleter
 interface Provider {
   suspend fun url(command: String, args: List<String>): GoResult
 
-//  suspend fun targets(command: String, args: List<String>): List<Target>
-
   suspend fun matches(command: String): Boolean = commandCompleter().matches(command)
 
   fun argumentCompleter(): ArgumentCompleter = SimpleArgumentCompleter(null)
