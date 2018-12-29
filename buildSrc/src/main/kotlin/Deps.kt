c319a858ed03db02bfd8c8a2c63cebd012e17c5f
Yuri Schimke <yuri@schimke.ee>
2018-12-29 12:43:04 +0000

    merge

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -1,10 +1,11 @@
 object Versions {
+  val conscrypt = "1.4.2"
   val kotlin = "1.3.11"
   val kotlinCoroutines = "1.1.0"
+  val ktor = "1.1.1"
   val logback = "1.2.3"
-  val ktor = "1.1.0"
-}
-
-object Deps {
-  val okio = "com.squareup.okio:okio:2.1.0"
+  val okhttp = "3.12.1"
+  val okio = "2.1.0"
+  val okurl = "1.68.0"
+  val reactor = "3.2.3.RELEASE"
 }
