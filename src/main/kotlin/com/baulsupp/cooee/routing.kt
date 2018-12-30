5dbf0c30655fbec1be2aca59f7c2693233d09651
Yuri Schimke <yuri@schimke.ee>
2018-12-30 12:21:08 +0000

    Cleanup

--- src/main/kotlin/com/baulsupp/cooee/routing.kt
+++ src/main/kotlin/com/baulsupp/cooee/routing.kt
@@ -17,11 +17,13 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.http.content.defaultResource
 import io.ktor.http.content.resources
 import io.ktor.http.content.static
+import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.locations.get
 import io.ktor.locations.post
 import io.ktor.response.respond
 import io.ktor.routing.Routing
 
+@KtorExperimentalLocationsAPI
 fun Routing.root(appServices: AppServices) {
   get<Login> { loginWeb(it, appServices.userStore) }
   get<Go> { bounceWeb(it, appServices.userServices.providersFor(call)) }
