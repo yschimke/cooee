6e821464508f4782bee785d53844838211ca8a5e
Yuri Schimke <yuri@schimke.ee>
2019-04-27 07:00:25 +0100

    Versions plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,12 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  kotlin("jvm") version Versions.kotlin
+  kotlin("jvm") version "1.3.21"
   war
   id("com.github.ben-manes.versions") version "0.21.0"
   id("com.google.cloud.tools.appengine") version "2.0.0-rc5"
-  id("org.jlleitschuh.gradle.ktlint") version "7.2.1"
+  id("com.palantir.consistent-versions") version "1.5.0"
+  id("com.diffplug.gradle.spotless") version "3.21.1"
 }
 
 group = "cooee"
@@ -58,69 +59,69 @@ tasks.create("downloadDependencies") {
 }
 
 dependencies {
-  implementation("ch.qos.logback:logback-classic:${Versions.logback}")
-  implementation("com.baulsupp:okurl:${Versions.okurl}")
-  implementation("com.google.cloud:google-cloud-logging-logback:0.79.0-alpha")
-  implementation("com.google.appengine:appengine-api-1.0-sdk:1.9.71")
-  implementation("io.lettuce:lettuce-core:5.1.5.RELEASE")
-  implementation("com.ryanharter.ktor:ktor-moshi:1.0.1")
-  implementation("com.squareup.moshi:moshi-adapters:1.8.0")
-  implementation("com.squareup.okhttp3:logging-interceptor:${Versions.okhttp}")
-  implementation("com.squareup.okhttp3:okhttp:${Versions.okhttp}")
-  implementation("com.squareup.okio:okio:${Versions.okio}")
-  implementation("io.ktor:ktor-auth-jwt:${Versions.ktor}")
-  implementation("io.ktor:ktor-auth-ldap:${Versions.ktor}")
-  implementation("io.ktor:ktor-auth:${Versions.ktor}")
-  implementation("io.ktor:ktor-client-auth-basic:${Versions.ktor}")
-  implementation("io.ktor:ktor-client-cio:${Versions.ktor}")
-  implementation("io.ktor:ktor-client-core-jvm:${Versions.ktor}")
-  implementation("io.ktor:ktor-client-core:${Versions.ktor}")
-  implementation("io.ktor:ktor-client-gson:${Versions.ktor}")
-  implementation("io.ktor:ktor-client-json-jvm:${Versions.ktor}")
-  implementation("io.ktor:ktor-client-okhttp:${Versions.ktor}")
-  implementation("io.ktor:ktor-client-websocket:${Versions.ktor}")
-  implementation("io.ktor:ktor-html-builder:${Versions.ktor}")
-  implementation("io.ktor:ktor-http-jvm:${Versions.ktor}")
-  implementation("io.ktor:ktor-http:${Versions.ktor}")
-  implementation("io.ktor:ktor-jackson:${Versions.ktor}")
-  implementation("io.ktor:ktor-locations:${Versions.ktor}")
-  implementation("io.ktor:ktor-metrics:${Versions.ktor}")
-  implementation("io.ktor:ktor-server-core:${Versions.ktor}")
-  implementation("io.ktor:ktor-server-host-common:${Versions.ktor}")
-  implementation("io.ktor:ktor-server-netty:${Versions.ktor}")
-  implementation("io.ktor:ktor-server-servlet:${Versions.ktor}")
-  implementation("io.ktor:ktor-server-sessions:${Versions.ktor}")
-  implementation("io.ktor:ktor-utils-jvm:${Versions.ktor}")
-  implementation("io.ktor:ktor-utils:${Versions.ktor}")
-  implementation("io.ktor:ktor-webjars:${Versions.ktor}")
-  implementation("io.ktor:ktor-websockets:${Versions.ktor}")
-  implementation("io.netty:netty-buffer:${Versions.netty}") { setForce(true) }
-  implementation("io.netty:netty-codec-dns:${Versions.netty}") { setForce(true) }
-  implementation("io.netty:netty-codec-http2:${Versions.netty}") { setForce(true) }
-  implementation("io.netty:netty-codec-http:${Versions.netty}") { setForce(true) }
-  implementation("io.netty:netty-codec:${Versions.netty}") { setForce(true) }
-  implementation("io.netty:netty-common:${Versions.netty}") { setForce(true) }
-  implementation("io.netty:netty-handler:${Versions.netty}") { setForce(true) }
-  implementation("io.netty:netty-resolver-dns:${Versions.netty}") { setForce(true) }
-  implementation("io.netty:netty-resolver:${Versions.netty}") { setForce(true) }
-  implementation("io.netty:netty-transport:${Versions.netty}") { setForce(true) }
-  implementation("io.projectreactor:reactor-core:${Versions.reactor}")
-  implementation("io.projectreactor:reactor-core:3.2.3.RELEASE")
-  implementation("org.conscrypt:conscrypt-openjdk-uber:${Versions.conscrypt}")
-  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:${Versions.kotlinCoroutines}")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:${Versions.kotlinCoroutines}")
-  implementation("org.jetbrains:kotlin-css-jvm:1.0.0-pre.68-kotlin-1.3.20")
-  implementation("org.mongodb:mongodb-driver-reactivestreams:1.11.0")
-  implementation("org.mongodb:mongodb-driver-sync:3.10.1")
-  implementation("org.webjars:jquery:3.2.1")
-  implementation("org.litote.kmongo:kmongo-coroutine:3.10.0")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug:${Versions.kotlinCoroutines}")
-  implementation("org.ff4j:ff4j-core:${Versions.ff4j}")
-  implementation("org.ff4j:ff4j-store-mongodb-v3:${Versions.ff4j}")
-  implementation("org.ff4j:ff4j-web:${Versions.ff4j}")
-  implementation("javax.servlet:javax.servlet-api:4.0.1")
-  testCompile("io.ktor:ktor-server-tests:${Versions.ktor}")
+  implementation("ch.qos.logback:logback-classic")
+  implementation("com.baulsupp:okurl")
+  implementation("com.google.cloud:google-cloud-logging-logback")
+  implementation("com.google.appengine:appengine-api-1.0-sdk")
+  implementation("io.lettuce:lettuce-core")
+  implementation("com.ryanharter.ktor:ktor-moshi")
+  implementation("com.squareup.moshi:moshi-adapters")
+  implementation("com.squareup.okhttp3:logging-interceptor")
+  implementation("com.squareup.okhttp3:okhttp")
+  implementation("com.squareup.okio:okio")
+  implementation("io.ktor:ktor-auth-jwt")
+  implementation("io.ktor:ktor-auth-ldap")
+  implementation("io.ktor:ktor-auth")
+  implementation("io.ktor:ktor-client-auth-basic")
+  implementation("io.ktor:ktor-client-cio")
+  implementation("io.ktor:ktor-client-core-jvm")
+  implementation("io.ktor:ktor-client-core")
+  implementation("io.ktor:ktor-client-gson")
+  implementation("io.ktor:ktor-client-json-jvm")
+  implementation("io.ktor:ktor-client-okhttp")
+  implementation("io.ktor:ktor-client-websocket")
+  implementation("io.ktor:ktor-html-builder")
+  implementation("io.ktor:ktor-http-jvm")
+  implementation("io.ktor:ktor-http")
+  implementation("io.ktor:ktor-jackson")
+  implementation("io.ktor:ktor-locations")
+  implementation("io.ktor:ktor-metrics")
+  implementation("io.ktor:ktor-server-core")
+  implementation("io.ktor:ktor-server-host-common")
+  implementation("io.ktor:ktor-server-netty")
+  implementation("io.ktor:ktor-server-servlet")
+  implementation("io.ktor:ktor-server-sessions")
+  implementation("io.ktor:ktor-utils-jvm")
+  implementation("io.ktor:ktor-utils")
+  implementation("io.ktor:ktor-webjars")
+  implementation("io.ktor:ktor-websockets")
+  implementation("io.netty:netty-buffer")
+  implementation("io.netty:netty-codec-dns")
+  implementation("io.netty:netty-codec-http2")
+  implementation("io.netty:netty-codec-http")
+  implementation("io.netty:netty-codec")
+  implementation("io.netty:netty-common")
+  implementation("io.netty:netty-handler")
+  implementation("io.netty:netty-resolver-dns")
+  implementation("io.netty:netty-resolver")
+  implementation("io.netty:netty-transport")
+  implementation("io.projectreactor:reactor-core")
+  implementation("io.projectreactor:reactor-core")
+  implementation("org.conscrypt:conscrypt-openjdk-uber")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
+  implementation("org.jetbrains:kotlin-css-jvm")
+  implementation("org.mongodb:mongodb-driver-reactivestreams")
+  implementation("org.mongodb:mongodb-driver-sync")
+  implementation("org.webjars:jquery")
+  implementation("org.litote.kmongo:kmongo-coroutine")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug")
+  implementation("org.ff4j:ff4j-core")
+  implementation("org.ff4j:ff4j-store-mongodb-v3")
+  implementation("org.ff4j:ff4j-web")
+  implementation("javax.servlet:javax.servlet-api")
+  testCompile("io.ktor:ktor-server-tests")
 }
 
 kotlin.sourceSets["main"].kotlin.srcDirs("src/main/kotlin")
@@ -141,5 +142,13 @@ dependencyUpdates.resolutionStrategy {
         reject("Alpha")
       }
     }
+  }
+}
+
+spotless {
+  kotlinGradle {
+    ktlint("0.31.0").userData(mutableMapOf("indent_size" to "2", "continuation_indent_size" to "2"))
+    trimTrailingWhitespace()
+    endWithNewline()
   }
 }
--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -1,13 +1,2 @@
 object Versions {
-  val conscrypt = "2.0.0"
-  val kotlin = "1.3.21"
-  val kotlinCoroutines = "1.1.1"
-  val ktor = "1.1.3"
-  val logback = "1.2.3"
-  val netty = "4.1.34.Final"
-  val okhttp = "3.14.0"
-  val okio = "2.2.2"
-  val okurl = "1.88.0"
-  val reactor = "3.2.6.RELEASE"
-  val ff4j = "1.8"
 }
