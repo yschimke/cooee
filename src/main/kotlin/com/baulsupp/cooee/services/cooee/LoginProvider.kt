33abd98ac525e36dcd1bf955c0e142862af68331
Yuri Schimke <yuri@schimke.ee>
2020-09-26 18:55:40 +0100

    Login impl

--- src/main/kotlin/com/baulsupp/cooee/services/cooee/LoginProvider.kt
+++ src/main/kotlin/com/baulsupp/cooee/services/cooee/LoginProvider.kt
@@ -13,11 +13,15 @@ import com.baulsupp.cooee.p.prefix
 import com.baulsupp.cooee.p.redirect
 import com.baulsupp.cooee.p.single_command
 import com.baulsupp.cooee.services.Provider
+import com.baulsupp.okurl.authenticator.AuthenticatingInterceptor
+import com.baulsupp.okurl.authenticator.authflow.*
+import com.baulsupp.okurl.authenticator.oauth2.Oauth2Flow
 import com.baulsupp.okurl.authenticator.oauth2.Oauth2ServiceDefinition
 import com.baulsupp.okurl.credentials.ServiceDefinition
 import com.baulsupp.okurl.services.strava.StravaAuthInterceptor
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import java.util.*
 
 class LoginProvider : Provider("login") {
   override suspend fun runCommand(request: CommandRequest): Flow<CommandResponse>? {
@@ -31,6 +35,59 @@ class LoginProvider : Provider("login") {
   }
 
   suspend fun login(service: String?): CommandResponse {
+    if (service == null) {
+      return login()
+    } else {
+      return loginService(service)
+    }
+  }
+
+  val config = mapOf(
+      "" to ""
+  )
+
+  private fun optionParams(serviceFlow: Oauth2Flow<*>, state: String): Map<String, Any> {
+    val options = serviceFlow.options()
+
+    println(options.map { it.param })
+
+    return options.map {
+      val value: Any = when (it) {
+        is Prompt -> config.get(it.param)?.toString() ?: ""
+        is Scopes -> config.get(it.param)?.split(",").orEmpty()
+        is Callback -> "http://localhost:8080/callback"
+        is State -> state
+      }
+
+      it.param to value
+    }.toMap()
+  }
+
+  suspend fun loginService(service: String): CommandResponse {
+    val flow = authFlow(service) ?: return CommandResponse.error("not found")
+
+    flow.init(client)
+
+    val state = UUID.randomUUID().toString()
+
+    val params = optionParams(flow as Oauth2Flow<*>, state)
+    flow.defineOptions(params)
+//    appServices.authenticationFlowCache.store(AuthenticationFlowInstance(state, token, request.service))
+
+    val url = flow.start()
+
+    println(url)
+
+    return CommandResponse.done("authed")
+  }
+
+  private fun authFlow(service: String): AuthFlow<out Any?>? {
+    return AuthenticatingInterceptor.defaultServices().find {
+      it.serviceDefinition.shortName() == service
+    }?.authFlow()
+  }
+
+  suspend fun login(): CommandResponse {
     val tokenResponse = clientApi.tokenRequest(TokenRequest(service = "cooee",
         login_url = "https://www.coo.ee/user/jwt?callback=http://localhost:3000/callback"))
 
