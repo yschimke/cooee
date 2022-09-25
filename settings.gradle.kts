c52efd7cd220362f04b4ee82b5e50d37b67a5754
Yuri Schimke <yuri@schimke.ee>
2022-09-25 08:59:09 +0100

    Cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,13 +5,4 @@ pluginManagement {
     gradlePluginPortal()
     mavenCentral()
   }
-  resolutionStrategy {
-    eachPlugin {
-      if (requested.id.id == "com.google.cloud.tools.appengine") {
-        useModule("com.google.cloud.tools:appengine-gradle-plugin:${requested.version}")
-      } else if (requested.id.id == "com.squareup.wire") {
-        useModule("com.squareup.wire:wire-gradle-plugin:${requested.version}")
-      }
-    }
-  }
 }
